de9a01891524f7765dcfcc51cf55daf2fc8f9e52
1-MANISH <91561321+1-MANISH@users.noreply.github.com>
2024-01-11 11:45:20 +0000

    Comparsion operators - special cases

---
+++ 01_basic/05_comparision.js
@@ -0,0 +1,24 @@
+// comparitsons operators
+
+// > , < ,  >= ,  <= , == ,=== , != 
+
+// before comparsion  - same datatypes should be compared
+
+
+// Don't Try below Given conversions
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+
+
+// all are false 
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+
+// === -> strict check  -value + dataTypes
