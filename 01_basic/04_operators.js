d2d5e1016436c8f596d3733624e3a53a67e66f6c
1-MANISH <91561321+1-MANISH@users.noreply.github.com>
2024-01-11 11:35:14 +0000

    Operations in Js

---
+++ 01_basic/04_operators.js
@@ -0,0 +1,37 @@
+// operations
+
+
+
+let value = 3
+let negativevalue = -value
+console.log(negativevalue);
+
+// +, - ,  * , / , ** , %
+
+
+let str1 = "Hello"
+let str2 = " Shanim"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1"+"2");
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+// according to ECMA script  - guildlines
+
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 110
+gameCounter++
+++gameCounter
+console.log(gameCounter);
+
+
+
